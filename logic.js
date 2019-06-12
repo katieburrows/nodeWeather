06f3d3833755ee599afb874a2168bc110f6c32cd
katieburrows <katie_burrows@me.com>
2019-06-12 15:38:34 -0600

    fs writing to file

--- logic.js
+++ logic.js
@@ -5,8 +5,25 @@ var fs = require('fs');
 weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result) {
   if(err) console.log(err);
  
-  console.log(JSON.stringify(result[0].current, null, 2));
+//   console.log(JSON.stringify(result[0].current, null, 2));
 });
+
+var commandLineArgs = process.argv.slice(2);
+
+    if (commandLineArgs[0].toLowerCase() === 'user'){
+        fs.appendFile('adminLog.txt', commandLineArgs.slice(1), function(error){
+            if (error) {
+                console.log(error);
+            } else console.log(`content added.`);
+        });
+        
+
+
+    } else if (commandLineArgs[0].toLowerCase() === 'admin'){
+        console.log(`admin hit`);
+    } else {
+        console.log(`not a valid entry, please choose: admin or user`);
+    }
 
 //collect cli args
 //sort into user or admin
