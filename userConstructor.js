8ed88e3feb95f1fc79dc4a23a654bcc85608a62f
katieburrows <katie_burrows@me.com>
2019-06-17 23:48:49 -0600

    fixed weather func

--- userConstructor.js
+++ userConstructor.js
@@ -5,13 +5,15 @@ var UserSearch = function(name, location) {
     this.location = location;
     this.date = Date.now();
 
-    weather.find({search: location, degreeType: "F"}, function(err, result) {
-        if(err) {
-            console.log(err);
-        }
-        
-        console.log(JSON.stringify(result[0].current, null, 2));
-    });
-}
+    this.weatherFunc = function() {
+        weather.find({search: location, degreeType: "F"}, function(err, result) {
+            if(err) {
+                console.log(err);
+            }
+            
+            console.log(JSON.stringify(result[0].current, null, 2));
+        });
+    };
+};
 
 module.exports = UserSearch;
