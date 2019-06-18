7d380c28f941e73d5fa162abb0469bb0be9332c7
katieburrows <katie_burrows@me.com>
2019-06-17 23:46:33 -0600

    moved fs function

--- adminConstructor.js
+++ adminConstructor.js
@@ -6,6 +6,14 @@ var WeatherAdmin = function(userName, searchedLocation, date) {
     this.userName = userName;
     this.searchedLocation = searchedLocation;
     this.date = date;
+
+            // fs.appendFile("adminLog.txt", user, function(error){
+        //     if (error) {
+        //         console.log(error);
+        //     } else {
+        //         console.log(`content added.`);
+        //     }
+        // });
 }
 
 module.exports = WeatherAdmin;
