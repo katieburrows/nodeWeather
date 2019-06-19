bf9828686e99cfc980494daf46d2f2f9d061f6bb
katieburrows <katie_burrows@me.com>
2019-06-18 22:39:28 -0600

    rearranged logic file

--- adminConstructor.js
+++ adminConstructor.js
@@ -3,17 +3,20 @@ var UserSearch = require("./userConstructor");
 var moment = require("moment");
 
 var WeatherAdmin = function(userName, searchedLocation, date) {
+    this.getData = function() {
+    fs.readFile("adminLog.txt", "utf8", function(error, data){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(data);
+            }
+        });
+    }
     this.userName = userName;
     this.searchedLocation = searchedLocation;
     this.date = date;
 
-            // fs.appendFile("adminLog.txt", user, function(error){
-        //     if (error) {
-        //         console.log(error);
-        //     } else {
-        //         console.log(`content added.`);
-        //     }
-        // });
+
 }
 
 module.exports = WeatherAdmin;
--- logic.js
+++ logic.js
@@ -2,18 +2,14 @@ var WeatherAdmin = require("./adminConstructor");
 
 var commandLineArgs = process.argv.slice(2);
 
+var admin = new WeatherAdmin();
+
 if (commandLineArgs[0].toLowerCase() === "user"){
     var location = commandLineArgs[2];      
 } 
 else if (commandLineArgs[0].toLowerCase() === "admin") {
-    var admin = new WeatherAdmin(userName, searchedLocation, date);
-    console.log(`admin hit`);
+    admin.getData();
 } 
 else {
     console.log(`not a valid entry, please choose: admin or user`);
 }
-
-//collect cli args
-//sort into user or admin
-    //if user use args to search weather-js
-    //if admin read adminLog.txt and console it
